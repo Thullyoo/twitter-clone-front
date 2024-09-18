69dbfa3dd4bb91253ccf95bf18b883d55b980220
Thullyoo <thullyofilho@gmail.com>
2024-09-18 10:57:24 -0300

    feat: created register component

---
+++ src/app/features/register/register.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss'
+})
+export class RegisterComponent {
+
+}
