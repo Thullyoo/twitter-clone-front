755eba6570407c62bb6e4c9f78c50466cde63479
Thullyoo <thullyofilho@gmail.com>
2024-09-18 10:56:54 -0300

    feat: added routes for login and register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './features/register/register.component';
+import { LoginComponent } from './features/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "register",
+        component: RegisterComponent
+    },
+    {
+        path: "login",
+        component: LoginComponent
+    },
+    {
+        path: "**",
+        component: RegisterComponent
+    }
+];
