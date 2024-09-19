c9126ce072253df17b3f2c79a266fd404977c833
Thullyoo <thullyofilho@gmail.com>
2024-09-19 12:08:06 -0300

    feat: interfaces for login

---
+++ src/app/features/login/interfaces/JwtResponse.ts
@@ -0,0 +1,4 @@
+export default interface JwtResponse{
+    acessToken: string,
+    expiresAt: number
+}
---
+++ src/app/features/login/interfaces/LoginRequest.ts
@@ -0,0 +1,4 @@
+export default interface LoginRequest {
+    nick: string,
+    password: string
+}
