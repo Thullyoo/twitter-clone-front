1ce7f4b754185e26ff64ad6b39a9010e6a907b9a
Thullyoo <thullyofilho@gmail.com>
2024-09-19 20:06:05 -0300

    feat finished login page

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { LoginService } from '../../shared/services/login.service';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import JwtResponse from './interfaces/JwtResponse';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  private loginService = inject(LoginService);
+
+  private formBuilder = inject(FormBuilder);
+
+  protected form = this.formBuilder.group({
+    username: new FormControl<string>("", {validators: Validators.required, nonNullable: true}),
+    password: new FormControl<string>("", {validators: Validators.required, nonNullable: true})
+  })
+
+  onLogin(event: Event){
+    event.preventDefault();
+    let jwt: JwtResponse = {
+      acessToken: "",
+      expiresAt: 0
+    }
+
+    this.loginService.postLogin({nick: this.form.controls.username.value, password: this.form.controls.password.value}) 
+    .subscribe({
+      next: (res) => {
+        jwt.acessToken = res.acessToken;
+        jwt.expiresAt = res.expiresAt;
+        console.log(jwt);
+      },
+      error: (error) =>{
+        console.log(error)
+      }
+      
+    })
+  }
 }
--- src/app/shared/services/login.service.ts
+++ src/app/shared/services/login.service.ts
@@ -12,32 +12,17 @@ export class LoginService {
 
   private HttpClient = inject(HttpClient);
 
-  private postLogin(request: LoginRequest){
-
-    let jwt: JwtResponse = {
-      acessToken: '',
-      expiresAt: 0
-    }
+  public postLogin(request: LoginRequest){
 
     const encodedCredentials = btoa(`${request.nick}:${request.password}`);
 
     const headerOptions = {
       headers: new HttpHeaders({
-        'Authorization': 'Basic ' + encodedCredentials;
+        'Authorization': 'Basic ' + encodedCredentials ,
       })
     }
-    this.HttpClient.post<JwtResponse>(this.url + "/login", null, headerOptions)
-    .subscribe({
-      next: (res) => {
-        jwt.acessToken = res.acessToken;
-        jwt.expiresAt = res.expiresAt;
-        return jwt
-      },
-      error: (error) =>{
-        console.log(error)
-      }
-      
-    })
+    return this.HttpClient.post<JwtResponse>(this.url + "/login", null, headerOptions);
+    
       
   }
 }
